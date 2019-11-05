9fa8a9878827f0b0964358dfe1094c63c23cc2e9
Ryan Kurte <ryankurte@gmail.com>
2019-11-05 14:21:09 +1300

    fixing config files

--- scripts/common.sh
+++ scripts/common.sh
@@ -11,6 +11,7 @@ function configure_file {
     -e "s|DOMAIN|$DOMAIN|g" \
     -e "s|EMAIL|$EMAIL|g" \
     -e "s|ROOT_NAME|$ROOT_NAME|g" \
+    -e "s|INT_NAME|$INT_NAME|g" \
     -e "s|COMMON_NAME|$3|g" \
     -e "s|DIR|$DIR|g" \
 	$1 > $2
