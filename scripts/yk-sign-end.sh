c8e599c2d54710f70771f09be675927a0e38479a
Ryan Kurte <ryankurte@gmail.com>
2019-11-19 11:06:46 +1300

    fixed extensions, now passed through to all certs

--- scripts/yk-sign-end.sh
+++ scripts/yk-sign-end.sh
@@ -6,8 +6,9 @@ OPENSSL_BIN=`which openssl`
 #OPENSSL_ENGINE="engine dynamic -pre SO_PATH:/usr/local/lib/engines/engine_pkcs11.so -pre ID:pkcs11 \
 #    -pre LIST_ADD:1 -pre LOAD -pre MODULE_PATH:/usr/local/lib/opensc-pkcs11.so"
 
-if [ "$#" -ne 3 ] && [ "$#" -ne 4 ]; then 
-    echo "Usage: $0 DIR INT_NAME END_NAME"
+if [ "$#" -ne 4 ]; then 
+    echo "Usage: $0 CA_NAME INT_NAME TYPE END_NAME"
+    echo "Where TYPE is client or server as appropriate"
     exit
 fi
 
@@ -32,13 +33,13 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     CONFIG="scripts/engine-osx.conf"
 fi
 
-echo "Signing end certificate: $DIR/$END_NAME.crt"
+echo "Signing end certificate: $DIR/$TYPE/$END_NAME.crt"
 
 set -e
 
 # Run sign command
 OPENSSL_CONF=$CONFIG openssl x509 -engine pkcs11 -CAkeyform engine -CAkey slot_0-id_2 \
-    -sha512 -CA $DIR/$INT_NAME.crt -req -in $DIR/$END_NAME.csr -days=$EXPIRY_DAYS \
-    -out $DIR/$END_NAME.crt
+    -sha512 -CA $DIR/$INT_NAME.crt -req -extensions v3_req -extfile $DIR/$TYPE/$END_NAME.conf \
+    -in $DIR/$TYPE/$END_NAME.csr -days=$EXPIRY_DAYS -out $DIR/$TYPE/$END_NAME.crt
 
-echo "Signed end certificate: $DIR/$END_NAME.crt"
+echo "Signed end certificate: $DIR/$TYPE/$END_NAME.crt"
