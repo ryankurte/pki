413eefb3e4152b59153fc88880f63425d0c9b339
Ryan Kurte <ryankurte@gmail.com>
2019-11-19 11:27:08 +1300

    fix yk-sign-end, add slot option to yk-load

--- scripts/yk-load.sh
+++ scripts/yk-load.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 # Load a certificate and key onto a yubikey
 
-if [ "$#" -ne 2 ]; then 
-    echo "Usage: $0 DIR NAME"
+if [ "$#" -ne 2 ] && [ "$#" -ne 3 ]; then 
+    echo "Usage: $0 DIR NAME [SLOT]"
     exit
 fi
 
 export DIR=$1
 export NAME=$2
+export SLOT=$3
+
+if [ "$SLOT" == "" ]; then
+export SLOT=9c
+fi
 
 set -e
 
 
 # First, load the desired key into slot 9c (note you may wish to set a touch-policy and pin-policy of never for embedded devices)
 echo "Loading key: $DIR/$NAME.key"
-ykman piv import-key 9c $DIR/$NAME.key --touch-policy=always --pin-policy=ONCE
+ykman piv import-key $SLOT $DIR/$NAME.key --touch-policy=always --pin-policy=ONCE
 
 
 # Second, load the certificate file into slot 9c
 echo "Loading cert: $DIR/$NAME.crt"
-ykman piv import-certificate 9c $DIR/$NAME.crt
+ykman piv import-certificate $SLOT $DIR/$NAME.crt
  
 
 # Report new device status
